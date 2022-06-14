18d171c65c5fd323ad2de2edfc961f78331a3314
sehno <orion.sehn@gmail.com>
2022-06-14 01:13:52 -0600

    tris

---
+++ triangles.py
@@ -0,0 +1,23 @@
+from graphics import *
+
+win = GraphWin("win", width=600, height=600)
+win.setCoords(-100, -100, 100, 100)
+cir = Circle(Point(3,3), 10 )
+cir.draw(win)
+
+
+tri_list = [
+    (0, 0, 0)
+]
+
+def eqtri(center, radius = 5):
+    return Polygon(())
+
+def spectri()
+
+for tri in tri_list:
+    eqtri().draw(win)
+
+win.getMouse()
+win.close()
+print("true")
